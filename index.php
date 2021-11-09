33028a716420595611d98ced67b2e86962eeaf89
Cohectum <cohectum@gmail.com>
2021-11-08 18:32:03 -0600

    Added base pages and base php
    added;
    
    -index.php
    -categories.php
    -createPost.php
    -DBconnect.php
    -DBCreateUpdate.php
    -editPost.php
    -editProfile.php
    -header.php
    -footer.php
    -login.php
    -messages.php
    -passwordReset.php
    -search.php
    -singlePost.php
    -viewUser.php
    and main.css

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Smorgasbord Trail - Home</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <link rel="stylesheet" href="main.css">
+    </head>
+    <body>
+        <div id="wrapper">
+            <?php include("header.php") ?>
+        </div>
+        
+
+    </body>
+</html>
