0f1c1766c229bbb3643ba3d4bc7de77ff5fae8ef
Cohectum <cohectum@gmail.com>
2021-11-08 19:29:35 -0600

    Building 11/8/2021
    - added about.php
    - added register.php
    - renamed header.php -> sidebar.php
    - added STLogo image
    
    - started building index.php
    -started initial css styling

--- index.php
+++ index.php
@@ -3,13 +3,17 @@
     <head>
         <title>Smorgasbord Trail - Home</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-        <link rel="stylesheet" href="main.css">
+        <link rel="stylesheet" href="./main.css">
     </head>
     <body>
         <div id="wrapper">
-            <?php include("header.php") ?>
-        </div>
-        
-
+            <?php include("sidebar.php") ?>
+            <h2>Welcome to Smorgasbord Trail!</h2>
+            <div class="split-text">
+                <p>Smorgasbord Trail is a website to buy and sell retro technology. A place for collectors and enthusiasts to come together to save these gems from landfills!</p>
+                <button id="about">Learn More</button>
+            </div> <!-- Close split-text Div 1 -->
+            <?php include("footer.php") ?>
+        </div> <!-- Close Wrapper Div -->
     </body>
 </html>
---
+++ sidebar.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <div id="sidebarBox">
+            <a href="./index.php"><img src="./Images/STLogo.png" alt="ST Logo" height=150></a>
+            <nav> 
+                <ul>
+                    <li><a href="./index.php">Home</a></li>
+                    <li><a href="./search.php">Search</a></li>
+                    <li><a href="./categories.php">Categories</a></li>
+                    <li><a href="./editProfile.php">Your Profile</a></li>
+                    <li><a href="./register.php">Register</a></li>
+                    <li><a href="./login.php">Log In</a></li>
+                </ul>
+            </nav>
+        </div>
+    </body> 
+</html>
